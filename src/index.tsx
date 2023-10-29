c67496e880a7ed5051a9c9a4a6bf318239cd0c8b
Angus Findlay <angus.findlay@icloud.com>
2023-10-29 08:12:45 +0000

    redirectkeepquery

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { serveStatic } from "hono/bun";
 
 import { Index } from "./components/Index";
@@ -19,6 +19,14 @@ let TODOS: Todo[] = [
 
 const app = new Hono();
 
+function redirectKeepQuery(
+  c: Context,
+  path: string
+): ReturnType<Context["redirect"]> {
+  const q = new URLSearchParams(c.req.query());
+  return c.redirect(path + "?" + q);
+}
+
 app.get("/", (c) => {
   const { filter } = c.req.query();
 
@@ -26,7 +34,6 @@ app.get("/", (c) => {
 });
 
 app.post("/todos", async (c) => {
-  const { filter } = c.req.query();
   const { todo: name } = await c.req.parseBody();
 
   const newTodo = {
@@ -37,61 +44,46 @@ app.post("/todos", async (c) => {
 
   TODOS = [newTodo, ...TODOS];
 
-  return c.redirect("/?filter=" + filter);
-});
-
-app.get("/todos/edit/:id", (c) => {
-  const { filter } = c.req.query();
-  const id = c.req.param("id");
-
-  const todo = TODOS.find((t) => t.id === id)!;
-
-  return c.redirect("/?filter=" + filter);
+  return redirectKeepQuery(c, "/");
 });
 
 app.post("/todos/toggle/:id", (c) => {
-  const { filter } = c.req.query();
   const { id } = c.req.param();
 
   const todo = TODOS.find((t) => t.id === id)!;
   todo.done = !todo.done;
 
-  return c.redirect("/?filter=" + filter);
+  return redirectKeepQuery(c, "/");
 });
 
 app.post("/todos/update/:id", async (c) => {
-  const { filter } = c.req.query();
   const { id } = c.req.param();
   const body = await c.req.parseBody();
 
   const todo = TODOS.find((t) => t.id === id)!;
   todo.name = typeof body.name == "string" ? body.name : "";
 
-  return c.redirect("/?filter=" + filter);
+  return redirectKeepQuery(c, "/");
 });
 
 app.post("/todos/delete/:id", (c) => {
-  const { filter } = c.req.query();
   const id = c.req.param("id");
   TODOS = TODOS.filter((t) => t.id !== id)!;
 
-  return c.redirect("/?filter=" + filter);
+  return redirectKeepQuery(c, "/");
 });
 
 app.post("/todos/clear-completed", (c) => {
-  const { filter } = c.req.query();
-
   TODOS = TODOS.filter((t) => !t.done);
 
-  return c.redirect("/?filter=" + filter);
+  return redirectKeepQuery(c, "/");
 });
 
 app.post("/todos/toggle-all", (c) => {
-  const { filter } = c.req.query();
   const all = TODOS.every((todo) => todo.done);
   TODOS.forEach((todo) => (todo.done = !all));
 
-  return c.redirect("/?filter=" + filter);
+  return redirectKeepQuery(c, "/");
 });
 
 app.use("/*", serveStatic({ root: "./assets/" }));
